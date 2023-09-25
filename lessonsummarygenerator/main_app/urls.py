bb17aee9223c8bb8d08adbd465e042a19ca46e54
jhsu79 <joshuanhsu@gmail.com>
2023-09-25 12:36:09 -0400

    add concept index page and begin concept detail page

--- lessonsummarygenerator/main_app/urls.py
+++ lessonsummarygenerator/main_app/urls.py
@@ -5,5 +5,7 @@ from .views import HomeView, AboutView
 
 urlpatterns = [
 	path('', HomeView.as_view(), name = 'home'), 
-    path('about/', AboutView.as_view(), name= 'about')
+    path('about/', AboutView.as_view(), name= 'about'),
+    path('concepts/', views.concepts_index, name='concepts_index'),
+    path('concepts/<int:concept_id>/', views.concept_detail, name='detail'),   
  ]
--- lessonsummarygenerator/main_app/views.py
+++ lessonsummarygenerator/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 
@@ -8,3 +8,19 @@ class HomeView(TemplateView):
 
 class AboutView(TemplateView): 
     template_name = 'about.html'
+
+
+concepts = [{'name': 'Intro to HTML', 'description': 'a basic lesson in how to build an html web page from scratch, understand the syntax and semantic elements of HTML, and correctly indent and nest html', 'keywords': ['HTML', 'Fundamentals', 'Syntax', 'Semantic' ,'fundamentals'], }, {'name': 'Intro to CSS', 'description': 'a basic lesson in how to style the elements on a web page, understand the css box model, and use css selectors to target elements for styling', 'keywords': ['box model', 'box-sizing', 'fundamentals', 'selectors', 'css']},{'name': 'Intro to Javascript', 'description':'An introduction to data types, variables, primitives in Javascript', 'keywords':['data types', 'primitives', 'javascript', 'fundamentals'],}]
+
+def concepts_index(request): 
+    return render(request, 'concepts/index.html', {'concepts': concepts})
+
+def concept_detail (request, concept_id):  
+    concept = concepts.get(id=concept_id) 
+    return render(request, 'concepts/detail.html', {'concepts': concept})
+
+# class ConceptsIndexView(ListView):  
+#     model = Concept
+#     def get_context_data(self, kwargs): 
+#         context = super().get_context_data(**kwargs) 
+#         return context
