4f2ed3a7ed8bb7123049a400f2fcc51f39343105
jhsu79 <joshuanhsu@gmail.com>
2023-09-30 23:07:36 -0400

    add final notes for views

--- main_app/views.py
+++ main_app/views.py
@@ -10,22 +10,23 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 import openai, os
 load_dotenv()
 
-
-# from .forms import LessonNoteForm
-# Create your views here.
-
-
+## All basic view functions start here 
+# Created Custom Views for Home and About Views. 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+# Create Customviews for certain pages. 
 class AboutView(TemplateView): 
     template_name = 'about.html'
 
+## All Concept view functions here. 
+#Will need a refactor into a class list view. 
 @login_required
 def concepts_index(request): 
     concepts = Concept.objects.all().order_by('name')
     return render(request, 'concepts/index.html', {'concepts': concepts})
 
+#Will need a refactor into a class detail view. 
 @login_required
 def concept_detail (request, concept_id):  
     concept = Concept.objects.get(id=concept_id) 
@@ -43,11 +44,14 @@ class ConceptDelete(LoginRequiredMixin,DeleteView):
       model = Concept
       success_url = '/concepts'
 
+## All Student view functions start here. 
+# Will need a refactor into a Class List View. 
 @login_required
 def students_index(request): 
     students = Student.objects.filter(user=request.user).order_by('last_name')
     return render(request, 'students/index.html', {'students':students})
 
+#Will need a refactor into a Class List View. 
 @login_required
 def student_detail(request, student_id):
     student = get_object_or_404(Student, id=student_id)
@@ -69,12 +73,14 @@ class StudentDelete(LoginRequiredMixin,DeleteView):
       model = Student
       success_url = '/students'
 
+## All lesson note view function here. 
+
 def lesson_note_detail(request, lesson_note_id, student_id):
     lesson_note = LessonNote.objects.get(id=lesson_note_id)
     student = get_object_or_404(Student, id=student_id)
     return render(request, 'students/lesson_note.html', {'Student': student,'lesson_note' : lesson_note})
 
-
+## All API call view function here. 
 def summarize_lesson_note(lesson_note_text):
         openai.api_key= os.getenv('OPEN_AI_KEY')
         prompt = lesson_note_text 
@@ -90,7 +96,7 @@ def summarize_lesson_note(lesson_note_text):
         print (response.choices)
         return summary
 
-
+ 
 class LessonNoteCreate(LoginRequiredMixin,CreateView): 
     model = LessonNote 
     form_class = LessonNoteForm
@@ -106,7 +112,7 @@ class LessonNoteCreate(LoginRequiredMixin,CreateView):
         # Restrict the queryset of the 'student' field to the current student
         form.fields['student'].queryset = Student.objects.filter(id=student.id)
         return form
-
+        ## Check to see if the form is valid and save. 
     def form_valid(self, form):
         student_id = self.kwargs['student_id']
         student = get_object_or_404(Student, id=student_id)       
@@ -121,9 +127,9 @@ class LessonNoteCreate(LoginRequiredMixin,CreateView):
         lesson_comments = form.cleaned_data['lesson_comments']
         assigned_homework = form.cleaned_data['assigned_homework']
         next_lesson_date = form.cleaned_data['next_lesson_date']
-
+        ## AI Prompt
         lesson_note_text =  f"Create a four paragraph summary addressed to {student}'s parents using the following structure: the first paragraph summarizes the {homework_accuracy_level}, {homework_completion_level}, and {homework_review_comments}, the second paragraph summarizes the {concepts_covered} and {lesson_comments}, the third paragraph creates an ordered list from the {assigned_homework}, and the fourth paragraph states the {next_lesson_date}"
-
+        ## Call the API and Save the Summary
         summary = summarize_lesson_note(lesson_note_text)
         self.object.lesson_summary = summary
         self.object.save()
@@ -148,6 +154,8 @@ class LessonNoteUpdate(LoginRequiredMixin,UpdateView):
 class LessonNoteDelete(LoginRequiredMixin,DeleteView):
       model = LessonNote
       success_url = '/students'
+
+## All sign up views here.  
 
 def signup(request):
   error_message = ''
